0e56322980ce7255249b8579af8839d5033f4842
瓜生敦也 <uryuatsuya@wlan-napt-005.u-aizu.ac.jp>
2023-06-09 11:46:12 +0900

    Dice game: initial revision

---
+++ dice-game.py
@@ -0,0 +1,15 @@
+import random
+
+def roll_dice():
+    return random.randint(1,6)
+
+def dice_game():
+    print("Rolling dice...")
+    die1 = roll_dice()
+    die2 = roll_dice()
+    print("Die 1: ",die1)
+    print("Die 2: ",die2)
+    total_value = die1 + die2
+    print("Toral value: ",total_value)
+
+dice_game()
